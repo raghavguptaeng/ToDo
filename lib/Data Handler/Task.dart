f498f1cce3334478c18d5ad7cd5f6bce4648f0c5
raghav gupta <raghavguptaeng@users.noreply.github.com>
2021-04-09 16:02:47 +0530

    back made working and snackbar added along with date sorting

--- lib/Data Handler/Task.dart
+++ lib/Data Handler/Task.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class Task{
   String _Name;
   String _Description;
@@ -9,10 +7,31 @@ class Task{
     this._Name = task;
     this._Description = desc;
     this._Deadline= deadline;
-    List temp = [this._Name,this._Description,this._Deadline,false];
-    _tasks.add(temp);
+    bool flag = false;
+    // loop below will be used to determine weather the task with same name is already present or not
+    for(int i=0 ; i<_tasks.length ; ++i){
+      if(task==_tasks[i][0]){
+        _tasks[i][0] = task;
+        _tasks[i][1] = desc;
+        _tasks[i][2] = deadline;
+        flag = true;
+        break;
+      }
+    }
+    //--------- if task not present , it will make a new task
+    if(flag == false){
+      List temp = [this._Name,this._Description,this._Deadline,false];
+      _tasks.add(temp);
+    }
   }
   List getData(){
+    // this will sort our list on the basis of dates
+    _tasks.sort((a,b) {
+      var ta = a[2];
+      var tb = b[2];
+      return ta.compareTo(tb);
+    });
+    //-----------------------------------
     return _tasks;
   }
 }
--- lib/Screens/Main Screen.dart
+++ lib/Screens/Main Screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/Data%20Handler/Task.dart';
 import 'package:todo/constants.dart';
@@ -12,9 +13,10 @@ class _MainScreenState extends State<MainScreen> {
   String displayforDeadline = "Deadline";
   String Taskname = '';
   String Description = '';
-  List taskList = [['','','']];
-  Task t = new Task();
-  bool isSelected = false;
+  List taskList = [
+    ['', '', '',false] // temp list to store data in main screen
+  ];
+  Task t = new Task(); // central class to store task values
   DateTime selectedDate = DateTime.now(); // for getting date
   final TextEditingController _taskNameController = new TextEditingController();
   final TextEditingController _descController = new TextEditingController();
@@ -34,11 +36,13 @@ class _MainScreenState extends State<MainScreen> {
         displayforDeadline = Date[2] + '-' + Date[1] + '-' + Date[0];
       });
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
   }
+
   //----------------------------------------------------------
   @override
   Widget build(BuildContext context) {
@@ -61,8 +65,15 @@ class _MainScreenState extends State<MainScreen> {
                         MaterialStateProperty.all<Color>(Colors.black)),
                 child: Text('ADD / SAVE'),
                 onPressed: () {
-                  t.insert(Taskname,displayforDeadline, Description);
+                  t.insert(Taskname, displayforDeadline, Description);
+                  final snackBar = SnackBar(
+                    content: Text('Task added / updated successfully'),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
                   setState(() {
+                    Taskname = '';
+                    displayforDeadline = 'Deadline';
+                    Description = '';
                     taskList = t.getData();
                   });
                 },
@@ -71,11 +82,27 @@ class _MainScreenState extends State<MainScreen> {
             Text('TODO', style: GoogleFonts.rubik(fontSize: 35)),
             Column(
                 children: taskList.map<Widget>((data) {
-              return new Row(
-                children: [
-                  Text(data[0]),
-                  Text(data[2])
-                ],
+              return GestureDetector(
+                onTap: (){
+                  setState(() {
+                    Taskname = data[0];
+                    displayforDeadline = data[2];
+                    Description = data[1];
+                  });
+                },
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      data[0],
+                      style: TextStyle(fontSize: 35, fontStyle: FontStyle.italic),
+                    ),
+                    Text(
+                      data[2],
+                      style: TextStyle(fontSize: 25, fontStyle: FontStyle.italic),
+                    ),
+                  ],
+                ),
               );
             }).toList())
           ],
@@ -83,7 +110,7 @@ class _MainScreenState extends State<MainScreen> {
       ),
     );
   }
-
+  // input widgets in a common method
   Column Inputs(BuildContext context) {
     return Column(
       children: [
